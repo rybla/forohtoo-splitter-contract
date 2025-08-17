6a543fa2d765c79b483e56c2436ffe0df4a47741
Henry Blanchette <henry@rybl.net>
2025-08-16 21:57:33 -0400

    setting up stuff

---
+++ build.sh
@@ -0,0 +1,3 @@
+pnpm hardhat test &&
+pnpm hardhat build &&
+echo "\n\n\nEverything looks ok! You may commit your changes now.";
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+# pnpx hardhat keystore set SEPOLIA_PRIVATE_KEY
+pnpx hardhat ignition deploy --network sepolia ignition/modules/Counter.ts
